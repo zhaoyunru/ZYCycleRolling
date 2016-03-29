6d12a0460065416236c76af93f99c8f8a09c37b0
yandeguangzy <754199572@qq.com>
2016-03-29 21:52:53 +0800

    Creat Project

---
+++ ZYCycleRolling/ZYCycleRolling/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYCycleRolling
+//
+//  Created by Dagan on 16/3/29.
+//  Copyright © 2016年 zyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
